588e93821b6db27d8e684d238378c10cf565a3a5
SaitovN <nuriksaitov03@gmailcom>
2023-06-02 16:59:43 +0600

    Create SumFrom1To100.java

---
+++ SumFrom1To100.java
@@ -0,0 +1,15 @@
+package academy.devonline.java.basic.section01_setup.section05_setup;
+
+public class SumFrom1To100 {
+    public static void main(String[] args) {
+        var a = 100;
+        var b = 1;
+        var s = 0;
+         for ( var i = b; i<=a ;i++){
+             s+=i;
+         }
+        System.out.println(s);
+
+    }
+
+}
